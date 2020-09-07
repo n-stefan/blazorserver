42218881dcac0cc27adbbc4a96785fe881f0ae04
Stefan Negulescu <tarthedark@gmail.com>
2020-09-07 19:09:17 +0300

    Improve db queries

--- Data/EfRepository.cs
+++ Data/EfRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,25 +8,28 @@ namespace Data
 {
     public class EfRepository<TEntity, TContext> : IRepository<TEntity> where TEntity : class, IEntity where TContext : DbContext
     {
-        private static readonly Func<TContext, int> _countQuery = EF.CompileQuery((TContext context) =>
-            context.Set<TEntity>().Count());
+        private static readonly Func<TContext, IEnumerable<int>> _allIdsQuery =
+            EF.CompileQuery((TContext context) => context.Set<TEntity>().Select(e => e.Id));
 
-        private static readonly Func<TContext, int, Task<TEntity>> _singleQuery = EF.CompileAsyncQuery((TContext context, int id) =>
-            context.Set<TEntity>().SingleOrDefault(e => e.Id == id));
+        private static readonly Func<TContext, int, Task<TEntity>> _entityByIdQuery =
+            EF.CompileAsyncQuery((TContext context, int id) => context.Set<TEntity>().SingleOrDefault(e => e.Id == id));
 
-        private readonly TContext _context;
         private readonly Random _random;
+        private readonly TContext _context;
+        private readonly int[] _ids;
 
         public EfRepository(TContext context)
         {
-            _context = context;
             _random = new Random();
+            _context = context;
+            //Assume no entities will be deleted/inserted
+            _ids = _allIdsQuery(_context).ToArray();
         }
 
         public Task<TEntity> GetRandom()
         {
-            var id = _random.Next(1, _countQuery(_context) + 1);
-            return _singleQuery(_context, id);
+            var index = _random.Next(0, _ids.Length);
+            return _entityByIdQuery(_context, _ids[index]);
         }
     }
 }
