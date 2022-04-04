5d1db5fd1688406935044ce6808aacda8b28997f
Stefan Negulescu <tarthedark@gmail.com>
2022-04-04 16:25:30 +0300

    Improve GraphQLCookieService

--- Services/GraphQLCookieService.cs
+++ Services/GraphQLCookieService.cs
@@ -12,22 +12,25 @@ public class GraphQLCookieService : ICookieService
     {
         try
         {
+            string message;
             var payload = new
             {
                 query = "{ randomCookie { id message } }"
             };
             var response = await _httpClient.PostAsJsonAsync("graphql", payload);
-            switch (response.StatusCode)
+            var content = await response.Content.ReadAsStringAsync();
+            var obj = JsonNode.Parse(content).AsObject();
+            if (obj.ContainsKey("errors"))
             {
-                case HttpStatusCode.OK:
-                    var content = await response.Content.ReadAsStringAsync();
-                    var obj = JsonNode.Parse(content).AsObject();
-                    var id = (int)obj["data"]["randomCookie"]["id"];
-                    var message = (string)obj["data"]["randomCookie"]["message"];
-                    return new CookieDto(id, message);
-                default:
-                    return new CookieDto(CookieDto.AnErrorOccurred);
-            };
+                message = (string)obj["errors"][0]["message"];
+                return new CookieDto(message);
+            }
+            else
+            {
+                var id = (int)obj["data"]["randomCookie"]["id"];
+                message = (string)obj["data"]["randomCookie"]["message"];
+                return new CookieDto(id, message);
+            }
         }
         catch (Exception)
         {
