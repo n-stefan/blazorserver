9cb47a5ab57822062fb00368d8a604db7d8a3086
Stefan Negulescu <tarthedark@gmail.com>
2022-04-13 18:56:24 +0300

    Upgrade Rest to current project template

--- Rest/Controllers/CookieController.cs
+++ Rest/Controllers/CookieController.cs
@@ -6,9 +6,13 @@ namespace Rest.Controllers;
 public class CookieController : ControllerBase
 {
     private readonly IRepository<Cookie> _repository;
+    private readonly ILogger<CookieController> _logger;
 
-    public CookieController(IRepository<Cookie> repository) =>
+    public CookieController(IRepository<Cookie> repository, ILogger<CookieController> logger)
+    {
         _repository = repository;
+        _logger = logger;
+    }
 
     [HttpGet("random")]
     public async Task<ActionResult<Cookie>> GetRandomCookie()
