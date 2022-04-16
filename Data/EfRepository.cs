622cd944b80b917f4c8d99a2c9216057e3e02ac4
Stefan Negulescu <tarthedark@gmail.com>
2022-04-16 17:10:34 +0300

    Improve EfRepository

--- Data/EfRepository.cs
+++ Data/EfRepository.cs
@@ -3,25 +3,25 @@ namespace Data;
 
 public abstract class EfRepository<TEntity, TContext> : IRepository<TEntity> where TEntity : class, IEntity where TContext : DbContext
 {
-    private static readonly Func<TContext, IEnumerable<int>> _allIdsQuery =
-        EF.CompileQuery((TContext context) => context.Set<TEntity>().Select(e => e.Id));
+    private static readonly Func<TContext, IAsyncEnumerable<int>> _allIdsQuery =
+        EF.CompileAsyncQuery((TContext context) => context.Set<TEntity>().Select(e => e.Id));
 
     private static readonly Func<TContext, int, Task<TEntity>> _entityByIdQuery =
         EF.CompileAsyncQuery((TContext context, int id) => context.Set<TEntity>().SingleOrDefault(e => e.Id == id));
 
     private readonly TContext _context;
-    private readonly int[] _ids;
 
-    protected EfRepository(TContext context)
-    {
+    protected EfRepository(TContext context) =>
         _context = context;
-        // Assume no entities will be deleted / inserted
-        _ids = _allIdsQuery(_context).ToArray();
-    }
 
-    public Task<TEntity> GetRandom()
+    public async Task<TEntity> GetRandom()
     {
-        var index = Random.Shared.Next(0, _ids.Length);
-        return _entityByIdQuery(_context, _ids[index]);
+        var ids = new List<int>();
+        await foreach (var id in _allIdsQuery(_context))
+        {
+            ids.Add(id);
+        }
+        var index = Random.Shared.Next(0, ids.Count);
+        return await _entityByIdQuery(_context, ids[index]);
     }
 }
