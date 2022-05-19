3ae60790e9b4fec1f1f37a51a4e60a3bb1d66e5c
Stefan Negulescu <tarthedark@gmail.com>
2022-05-19 20:32:13 +0300

    Improve tests

--- tests/BlazorServer.UnitTests/Core/Services/DirectCookieService_GetRandomCookie.cs
+++ tests/BlazorServer.UnitTests/Core/Services/DirectCookieService_GetRandomCookie.cs
@@ -3,6 +3,21 @@ namespace BlazorServer.UnitTests.Core.Services;
 
 public class DirectCookieService_GetRandomCookie
 {
+  private static readonly string[] _cookies =
+  {
+    "fortune: No such file or directory",
+    "A computer scientist is someone who fixes things that aren't broken.",
+    "After enough decimal places, nobody gives a damn.",
+    "A bad random number generator: 1, 1, 1, 1, 1, 4.33e+67, 1, 1, 1",
+    "A computer program does what you tell it to do, not what you want it to do.",
+    "Emacs is a nice operating system, but I prefer UNIX. — Tom Christaensen",
+    "Any program that runs right is obsolete.",
+    "A list is only as strong as its weakest link. — Donald Knuth",
+    "Feature: A bug with seniority.",
+    "Computers make very fast, very accurate mistakes.",
+    "<script>alert('This should not be displayed in a browser alert box.');</script>",
+    "フレームワークのベンチマーク"
+  };
   private readonly Mock<IRepository<Cookie>> _mockRepository = new();
   private readonly DirectCookieService _directCookieService;
 
@@ -36,7 +51,8 @@ public class DirectCookieService_GetRandomCookie
   public async Task ReturnsCookieDto_Given_CookieFound()
   {
     string? expectedErrorMessage = null;
-    var cookie = new Cookie { Id = 1, Message = "fortune: No such file or directory" };
+    var index = Random.Shared.Next(0, _cookies.Length);
+    var cookie = new Cookie { Id = index + 1, Message = _cookies[index] };
     _mockRepository.Setup(r => r.GetRandom()).ReturnsAsync(cookie);
 
     var result = await _directCookieService.GetRandomCookie();
