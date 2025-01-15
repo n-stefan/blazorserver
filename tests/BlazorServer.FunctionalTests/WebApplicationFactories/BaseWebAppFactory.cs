4257df7f5b9e3b5ecd977206603e62c64076dc81
Stefan Negulescu <tarthedark@gmail.com>
2025-01-15 21:40:16 +0200

    Make functional tests more robust

--- tests/BlazorServer.FunctionalTests/WebApplicationFactories/BaseWebAppFactory.cs
+++ tests/BlazorServer.FunctionalTests/WebApplicationFactories/BaseWebAppFactory.cs
@@ -3,17 +3,29 @@ namespace BlazorServer.FunctionalTests.WebApplicationFactories;
 
 public abstract class BaseWebAppFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
 {
+  private readonly IHost _fakeHost;
+
+  protected BaseWebAppFactory()
+  {
+    var server = new TestServer(new WebHostBuilder().UseStartup<FakeStartup>());
+#pragma warning disable EXTEXP0016
+    _fakeHost = FakeHost.CreateBuilder().ConfigureServices(x => x.AddSingleton<IServer>(_ => server)).Build();
+#pragma warning restore EXTEXP0016
+  }
+
   protected override IHost CreateHost(IHostBuilder builder)
   {
     ArgumentNullException.ThrowIfNull(builder);
 
-    var testHost = builder.Build();
-
-    builder.ConfigureWebHost(webHostBuilder => webHostBuilder.UseKestrel());
-
+    builder.ConfigureWebHost(x => x.UseKestrelCore());
     var host = builder.Build();
     host.Start();
 
-    return testHost;
+    return _fakeHost;
   }
+}
+
+public class FakeStartup
+{
+  public void Configure() { }
 }
